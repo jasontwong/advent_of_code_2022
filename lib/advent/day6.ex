58af33a51c6574bf10ae21f05ff283237d92a0d4
Jason T. Wong <jasontwong@users.noreply.github.com>
2022-12-06 09:33:26 -0500

    Day 6

---
+++ lib/advent/day6.ex
@@ -0,0 +1,38 @@
+defmodule Advent.Day6 do
+  def answer_p1 do
+    get_start_marker(get_data(), 5)
+  end
+
+  def answer_p2 do
+    get_start_marker(get_data(), 14)
+  end
+
+  defp get_data() do
+    {:ok, data} =
+      :advent
+      |> :code.priv_dir()
+      |> Path.join("day6_input.txt")
+      |> File.read()
+
+    data
+    |> String.trim()
+    |> String.codepoints()
+  end
+
+  def get_start_marker(message, stop, index \\ 0, marker \\ [])
+
+  def get_start_marker(_message, stop, index, marker) when length(marker) == stop, do: index
+
+  def get_start_marker([hd | message], stop, index, marker) do
+    marker =
+      marker
+      |> Enum.find_index(fn letter -> letter == hd end)
+      |> case do
+        nil -> marker
+        index -> Enum.slice(marker, (index + 1)..-1)
+      end
+      |> List.insert_at(-1, hd)
+
+    get_start_marker(message, stop, index + 1, marker)
+  end
+end
--- test/advent_test.exs
+++ test/advent_test.exs
@@ -2,7 +2,39 @@ defmodule AdventTest do
   use ExUnit.Case
   doctest Advent
 
-  test "greets the world" do
-    assert Advent.hello() == :world
+  describe "day6" do
+    test "get_start_marker 4 distinct characters" do
+      message = String.codepoints("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+      assert Advent.Day6.get_start_marker(message, 4) == 7
+
+      message = String.codepoints("bvwbjplbgvbhsrlpgdmjqwftvncz")
+      assert Advent.Day6.get_start_marker(message, 4) == 5
+
+      message = String.codepoints("nppdvjthqldpwncqszvftbrmjlhg")
+      assert Advent.Day6.get_start_marker(message, 4) == 6
+
+      message = String.codepoints("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+      assert Advent.Day6.get_start_marker(message, 4) == 10
+
+      message = String.codepoints("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+      assert Advent.Day6.get_start_marker(message, 4) == 11
+    end
+
+    test "get_start_marker 14 distinct characters" do
+      message = String.codepoints("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+      assert Advent.Day6.get_start_marker(message, 14) == 19
+
+      message = String.codepoints("bvwbjplbgvbhsrlpgdmjqwftvncz")
+      assert Advent.Day6.get_start_marker(message, 14) == 23
+
+      message = String.codepoints("nppdvjthqldpwncqszvftbrmjlhg")
+      assert Advent.Day6.get_start_marker(message, 14) == 23
+
+      message = String.codepoints("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+      assert Advent.Day6.get_start_marker(message, 14) == 29
+
+      message = String.codepoints("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+      assert Advent.Day6.get_start_marker(message, 14) == 26
+    end
   end
 end
